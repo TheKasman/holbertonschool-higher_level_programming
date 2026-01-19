1dae329662cf3dd72e950f48deea4140173012c6
TheKasman <kasapidisandrew@gmail.com>
2026-01-19 03:24:35 +0000

    changed it so it only uses one variable. thanks str.split!

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-words = str.split()
-sentence = " ".join(words[5:7] + [words[-4]] + [words[0]])
-print(sentence)
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = " ".join(str.split()[5:7] + [str.split()words[-4]] + [str.split()words[0]])
+print(str)
