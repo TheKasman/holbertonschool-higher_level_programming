7ce27e4700656b202eef3c0a9cd3ed7e094d52db
TheKasman <kasapidisandrew@gmail.com>
2026-01-19 03:21:28 +0000

    fixed the line issue

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 words = str.split()
 sentence = " ".join(words[5:7] + [words[-4]] + [words[0]])
 print(sentence)
