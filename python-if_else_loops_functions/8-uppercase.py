cc5424c381702588134bd820519ea7373057f355
TheKasman <kasapidisandrew@gmail.com>
2026-01-21 03:20:38 +0000

    typos fixed

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in str:
         if 'a' <= i <= 'z':
-            i = chr(ord((i) - 32))
-        print({}.format(i), end="")
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
     print()
