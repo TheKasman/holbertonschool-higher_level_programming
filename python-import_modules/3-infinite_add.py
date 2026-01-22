303dce5f6db8811c9fc3536a9205b67a270a502a
TheKasman <kasapidisandrew@gmail.com>
2026-01-22 13:03:31 +0000

    remember to initialise the variables

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,6 @@ import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
     for i in range(1, len(sys.argv)):
+        result = 0
         result += int(sys.argv[i])
         print("{}".format(result))
