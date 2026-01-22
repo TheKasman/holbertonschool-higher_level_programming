b8f8f737f8c4c5526a8f9b1a27098c887f5a34f5
TheKasman <kasapidisandrew@gmail.com>
2026-01-22 12:57:32 +0000

    sys.argv[i] IS A STRING NOT THE LENGTH HAHA

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
-    for i in range(1, len(sys.argv[i])):
+    for i in range(1, len(sys.argv)):
         result = int(sys.argv[i]) + int(sys.argv[i + 1])
         print("{}".format(result))
