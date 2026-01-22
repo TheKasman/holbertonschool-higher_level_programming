c0e2b0e5917ba7e34fe41b34948c35880f51cd5f
TheKasman <kasapidisandrew@gmail.com>
2026-01-22 13:05:16 +0000

    one line, not each time

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     result = 0
     for i in range(1, len(sys.argv)):
         result += int(sys.argv[i])
-        print("{}".format(result))
+    print("{}".format(result))
