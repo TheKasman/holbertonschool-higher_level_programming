d4db04df7f5b6560ef114af3d57a1c3eb211bc76
TheKasman <kasapidisandrew@gmail.com>
2026-01-22 12:41:32 +0000

    huh... it's just a list

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
-        print("{}".format(sys.argv[1]))
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
