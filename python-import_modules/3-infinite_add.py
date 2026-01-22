01b57f7f49279ef839c46f5c3d466f0cfa5231e3
TheKasman <kasapidisandrew@gmail.com>
2026-01-22 13:01:07 +0000

    bad addition logic. fixed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
     for i in range(1, len(sys.argv)):
-        result = int(sys.argv[i]) + int(sys.argv[i + 1])
+        result += int(sys.argv[i])
         print("{}".format(result))
