e871baad6ac4f37218a180ab5c1dc2f2b75a671b
TheKasman <kasapidisandrew@gmail.com>
2026-01-22 13:04:29 +0000

    don't reset result each time

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
+    result = 0
     for i in range(1, len(sys.argv)):
-        result = 0
         result += int(sys.argv[i])
         print("{}".format(result))
