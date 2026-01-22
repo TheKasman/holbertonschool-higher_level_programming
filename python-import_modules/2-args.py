d344849f00248d74f193332137919dc5fc523277
TheKasman <kasapidisandrew@gmail.com>
2026-01-22 12:39:35 +0000

    huh... it's just a list

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+count = len(sys.argv) - 1
+
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
+    print("{}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
