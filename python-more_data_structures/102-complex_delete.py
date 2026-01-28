ed51a91143604c721e98a153472a46c9c09fa7fd
TheKasman <kasapidisandrew@gmail.com>
2026-01-28 03:31:25 +0000

    weird but okay

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if not a_dictionary:
+        return a_dictionary
+
+    keysToDelete = [key for key, val in a_dictionary.items() if val == value]
+
+    for k in keysToDelete:
+        del a_dictionary[k]
+
+    return a_dictionary
