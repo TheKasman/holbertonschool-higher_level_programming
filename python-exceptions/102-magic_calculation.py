475c59d4fe4b5f3547132d8b427d5c03d0cfdd6f
TheKasman <kasapidisandrew@gmail.com>
2026-01-29 23:03:51 +0000

    i hate bytecode

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -8,7 +8,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except Exception:
-            pass
-        result = b + a
+        except:
+            result = b + a
+            break
     return result
