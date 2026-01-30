142292e2dc4e437285f94484b48e66aa2c475ce8
TheKasman <kasapidisandrew@gmail.com>
2026-01-30 03:13:21 +0000

    question 0 done?

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Adds two integers together and returns the result
+a and b must be integers (floats are truncated to integers)
+
+>>> add_integer(1, 2)
+3
+>>> add_integer(100, -2)
+98
+>>> add_integer(2)
+100
+>>> add_integer(100.3, -2)
+98
+>>> add_integer(4, "School")
+Traceback (most recent call last):
+TypeError: b must be an integer
+>>> add_integer(None)
+Traceback (most recent call last):
+TypeError: a must be an integer
+"""
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
