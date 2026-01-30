3f0a489b5cd4080122fa3d59c20a3cf486e8997c
TheKasman <kasapidisandrew@gmail.com>
2026-01-30 13:15:45 +0000

    reverted to a very early version

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -26,18 +26,8 @@ def add_integer(a, b=98):
     Traceback (most recent call last):
     TypeError: a must be an integer
     """
-    if isinstance(a, str):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, str):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    try:
-        a = int(a)
-    except (TypeError, ValueError, OverflowError):
-        raise TypeError("a must be an integer")
-
-    try:
-        b = int(b)
-    except (TypeError, ValueError, OverflowError):
-        raise TypeError("b must be an integer")
-
-    return a + b
+    return int(a) + int(b)
