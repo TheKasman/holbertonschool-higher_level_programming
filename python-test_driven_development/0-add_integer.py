f53fbd6d43fbd65f8182f8f94f798df6c922ff2a
TheKasman <kasapidisandrew@gmail.com>
2026-01-30 09:48:21 +0000

    changed return to a + b and put the cast in early

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -26,18 +26,19 @@ def add_integer(a, b=98):
     Traceback (most recent call last):
     TypeError: a must be an integer
     """
-    # Reject Nan/inf... man what even
-    if isinstance(a, float) and (a != a or a == float('inf')
-                                 or a == float('-inf')):
-        raise TypeError("a must be an integer")
-    if isinstance(b, float) and (b != b or b == float('inf')
-                                 or b == float('-inf')):
-        raise TypeError("b must be an integer")
-    # Type checks
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    try:
+        a = int(a)
+    except (ValueError, OverflowError):
+        raise TypeError("a must be an integer")
+
+    try:
+        b = int(b)
+    except (ValueError, OverflowError):
+        raise TypeError("b must be an integer")
+
+    return a + b
