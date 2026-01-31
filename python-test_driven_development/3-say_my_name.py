b0dc33cb087c4487e9efe1b0f2c4b612ff646878
TheKasman <kasapidisandrew@gmail.com>
2026-01-31 01:36:59 +0000

    changed the order of tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -22,15 +22,14 @@ def say_my_name(first_name, last_name=""):
     >>> say_my_name("Bob")
     My name is Bob
 
-    >>> say_my_name()
-    Traceback (most recent call last):
-    TypeError: say_my_name() missing 1
-      required positional argument: 'first_name'
-
     >>> say_my_name("Bob", 12)
     Traceback (most recent call last):
     TypeError: first_name must be a string
 
+    >>> say_my_name()
+    Traceback (most recent call last):
+    TypeError: say_my_name() missing 1
+      required positional argument: 'first_name'
     """
 
     if not isinstance(first_name, str):
