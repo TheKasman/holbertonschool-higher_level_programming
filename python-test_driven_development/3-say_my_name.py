5007229bef8a34cdd115480df98440c67c4c61a9
TheKasman <kasapidisandrew@gmail.com>
2026-01-31 01:39:47 +0000

    typo!

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -24,7 +24,7 @@ def say_my_name(first_name, last_name=""):
 
     >>> say_my_name("Bob", 12)
     Traceback (most recent call last):
-    TypeError: first_name must be a string
+    TypeError: last_name must be a string
 
     >>> say_my_name()
     Traceback (most recent call last):
@@ -35,5 +35,5 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError("first_name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
