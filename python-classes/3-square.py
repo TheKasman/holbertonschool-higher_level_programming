782f2302344d8ef1629285cb99f74846482748ee
TheKasman <kasapidisandrew@gmail.com>
2026-02-01 23:55:14 +0000

    moved the docstring for area inside the function and changed the output

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,6 +12,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    """Prints the current area of the square"""
     def area(self):
-        print(self.__size)
+        """Prints the current area of the square"""
+        return self.__size ** 2
