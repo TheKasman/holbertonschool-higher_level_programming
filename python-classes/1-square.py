cab775004460016a813737b44b436f70e9924239
TheKasman <kasapidisandrew@gmail.com>
2026-02-01 23:33:13 +0000

    that should do it

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines the Square class again"""
+
+
+class Square:
+    """The Square class NOW WITH SIZE"""
+    def __init__(self, size):
+        """Constructor for a new square"""
+        self.__size = size
