e91f49a94e789cba01677be94f60c1c8109e88e7
TheKasman <kasapidisandrew@gmail.com>
2026-02-02 00:56:35 +0000

    weird

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """Calculates and returns the current area of the square"""
