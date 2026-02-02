3960e477614edf13140ec074a7f620ae5588e606
TheKasman <kasapidisandrew@gmail.com>
2026-02-02 00:27:58 +0000

    getter and setter... got and set

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines the Square class again.. again?"""
+
+
+class Square:
+    """The Square class NOW WITH DEFINED SIZE"""
+    def __init__(self, size=0):
+        """Constructor for a new square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Prints the current area of the square"""
+        return self.__size ** 2
+
+    #  SIZE PROPERTY
+    @property
+    def size(self):
+        """Returns the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
