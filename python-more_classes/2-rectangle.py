9b5ed443eaa02b997d50fcda34d7776295864388
TheKasman <kasapidisandrew@gmail.com>
2026-02-03 00:08:05 +0000

    forgot to return 0 on width/height == 0

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -44,4 +44,6 @@ class Rectangle:
 
     def perimeter(self):
         """Returns the preimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
         return (self.width * 2) + (self.height * 2)
