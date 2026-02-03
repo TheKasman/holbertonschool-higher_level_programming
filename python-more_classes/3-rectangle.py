5d9fd277b009718028fc3cfb6ba1240651c23bac
TheKasman <kasapidisandrew@gmail.com>
2026-02-03 00:27:34 +0000

    i can spell

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Docstring for the Rectangle class
+"""
+
+
+class Rectangle:
+    """The Rectangle class with width and height properties"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle - with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle - with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the preimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width * 2) + (self.height * 2)
+
+    def print(self):
+        if self.height == 0 or self.width == 0:
+            return ""
+        print(("#" * self.width + "\n") * self.height, end="")
+
+    def __str__(self):
+        return ("#" * self.width + "\n") * self.height
