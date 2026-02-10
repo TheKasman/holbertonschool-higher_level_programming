c3a74b6d083902b62c314cb8b07aa0fee4dc6026
TheKasman <kasapidisandrew@gmail.com>
2026-02-10 01:44:12 +0000

    not if it's exactly class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ Lists the answer to the function that inherits_from a class
 
 def inherits_from(obj, a_class):
     """returns true if the object's class matches the search case"""
-    return issubclass(obj.__class__, a_class)
+    return issubclass(obj.__class__, a_class) and obj.__class__ is not a_class
