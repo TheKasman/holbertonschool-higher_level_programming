22a78135fb6d867a661c3e02916dd00a0f40e3dc
TheKasman <kasapidisandrew@gmail.com>
2026-02-10 01:53:30 +0000

    well.. okay

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Base Geometry functions/classes coming in"""
+
+
+class BaseGeometry():
+    """Base Geometry"""
+    def area(self):
+        """returns the area of the geometric shape"""
+        raise Exception("area() is not implemented")
