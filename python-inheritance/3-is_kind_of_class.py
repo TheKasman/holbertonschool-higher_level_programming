84c87ab554bcdd3f15aa9aded0befcdcfab8e599
TheKasman <kasapidisandrew@gmail.com>
2026-02-10 00:49:55 +0000

    that's the instance one

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Answer for the third question"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks to see if the object is the same as the specified class"""
+    return isinstance(obj, a_class)
