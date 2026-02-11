a010965993a003d7cb7ac003748a261d88d38571
TheKasman <kasapidisandrew@gmail.com>
2026-02-11 01:24:18 +0000

    typo

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@
     width and height must be private. No getter or setter
     width and height must be positive integers, validated by integer_validator
 """
-BaseGeometry = __import__('7-BaseGeometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
