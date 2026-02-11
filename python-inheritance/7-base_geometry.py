1abaea5c33e6a5ed620f767ffc2f48dc9a0607e7
TheKasman <kasapidisandrew@gmail.com>
2026-02-11 00:27:58 +0000

    first attempt

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Base Geometry functions/classes coming in"""
+
+
+class BaseGeometry():
+    """Base Geometry"""
+    def area(self):
+        """returns the area of the geometric shape"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that an integer is being parsed"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
