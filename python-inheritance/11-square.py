4e957486a4e95d278f91b872204c28e065e147ab
TheKasman <kasapidisandrew@gmail.com>
2026-02-11 01:46:37 +0000

    Be there or be square cause you ain't around

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Write a class Square that inherits from Rectangle (9-rectangle.py):
+
+    Instantiation with size: def __init__(self, size)::
+    size must be private. No getter or setter
+    size must be a positive integer, validated by integer_validator
+    the area() method must be implemented
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This one inherits from rectangle. Big waterfall"""
+    def __init__(self, size):
+        """Constructor"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__width, self.__height)
