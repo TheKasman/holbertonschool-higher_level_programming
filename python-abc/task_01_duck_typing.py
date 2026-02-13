86fdc69258bebbfa17fd0c0a8e90f116a4bf4d7b
TheKasman <kasapidisandrew@gmail.com>
2026-02-13 00:55:46 +0000

    okay underengineering time

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -3,7 +3,6 @@
 """Abstract classes task 0"""
 
 from abc import ABC, abstractmethod
-import math
 
 
 class Shape(ABC):
@@ -18,15 +17,10 @@ class Shape(ABC):
         """the perimeter method"""
         pass
 
-
 class Circle(Shape):
     """Circle class"""
     def __init__(self, radius):
         """constructor"""
-        if not isinstance(radius, (int, float)):
-            raise TypeError("radius must be an number")
-        if radius <= 0:
-            raise ValueError("radius must be > 0")
         self.radius = radius
 
     def area(self):
@@ -42,13 +36,6 @@ class Rectangle(Shape):
     """Rectangle class"""
     def __init__(self, width, height):
         """constructor"""
-        if not isinstance(width, (int, float)):
-            raise TypeError("width must be an number")
-        if not isinstance(height, (int, float)):
-            raise TypeError("height must be an number")
-        if width <= 0 or height <= 0:
-            raise ValueError("Width and height must be > 0")
-
         self.width = width
         self.height = height
 
@@ -59,3 +46,7 @@ class Rectangle(Shape):
     def perimeter(self):
         """returns 2(x + y)"""
         return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
