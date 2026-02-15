6676787008b6a95d89972f7fd04d468bc3c1db25
TheKasman <kasapidisandrew@gmail.com>
2026-02-15 23:43:14 +0000

    write.. but also wrong? xD

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,3 +7,4 @@ def write_file(filename="", text=""):
     """I would never have guessed what this does!?"""
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
+    return text
