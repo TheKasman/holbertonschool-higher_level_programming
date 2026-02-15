11a6a52d2c19c6333935435e78a910fda8f0795f
TheKasman <kasapidisandrew@gmail.com>
2026-02-15 23:46:01 +0000

    returns an int

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@
 def write_file(filename="", text=""):
     """I would never have guessed what this does!?"""
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
-    return text
+        num_chars = f.write(text)
+    return num_chars
