204d0e8ac30be6bae4dd3a6884061ece8fb0987b
TheKasman <kasapidisandrew@gmail.com>
2026-02-16 03:38:23 +0000

    what a mind boggle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """Now we load add and save things...importing all the other files required"""
-import json
+import sys
 saved = __import__('5-save_to_json_file').save_to_json_file
 loaded = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(text=""):
+def add_item():
     """Adding items"""
+    args = sys.argv[1:]
     data = loaded("add_item.json")
 
     if data is None:
         data = []
 
-    data.append(text)
+    for x in args:
+        data.append(x)
 
     saved(data, "add_item.json")
