57cb960fff7b63814d55482cbc103863330a1a6c
TheKasman <kasapidisandrew@gmail.com>
2026-02-16 03:23:49 +0000

    modules being imported now no longer have ()

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Now we load add and save things...importing all the other files required"""
 import json
-saved = __import__('5-save_to_json_file').save_to_json_file()
-loaded = __import__('6-load_from_json_file').load_from_json_file()
+saved = __import__('5-save_to_json_file').save_to_json_file
+loaded = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_item(text=""):
