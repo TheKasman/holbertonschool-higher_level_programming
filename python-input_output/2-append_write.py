128851a25bd2671443d0101628eeb63f88552b82
TheKasman <kasapidisandrew@gmail.com>
2026-02-16 00:01:54 +0000

    OH NO I FORGOT TO CHANGE THE FUNCTION NAME

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 """This file, append to it. It literally appends a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Append to it"""
     with open(filename, "a", encoding="utf-8") as f:
         num_chars = f.write(text)
