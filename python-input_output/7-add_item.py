b82dd6a4e2badd199c4fe80a4059f2880f5a8fcb
TheKasman <kasapidisandrew@gmail.com>
2026-02-16 03:20:12 +0000

    moved the module comment lower

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Now we load add and save things...
-importing all the other files required"""
 import json
 saved = __import__('5-save_to_json_file').save_to_json_file()
 loaded = __import__('6-load_from_json_file').load_from_json_file()
+"""Now we load add and save things...
+importing all the other files required"""
 
 
 def add_item(text=""):
