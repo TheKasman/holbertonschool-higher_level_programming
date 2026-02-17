a80cb22020ee59e0ac7df6a0352eb3a04ce267cb
TheKasman <kasapidisandrew@gmail.com>
2026-02-17 01:56:21 +0000

    parentheses!

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,8 +12,8 @@ def pascal_triangle(n):
             if i == 0 or i == row_n:
                 current_value = 1
             else:
-                current_value = triangle[row_n - 1][i - 1]
-                + triangle[row_n - 1][i]
+                current_value = (triangle[row_n - 1][i - 1]
+                                 + triangle[row_n - 1][i])
 
             row.append(current_value)
         triangle.append(row)
