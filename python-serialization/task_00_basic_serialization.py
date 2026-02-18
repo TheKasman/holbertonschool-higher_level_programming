3a8ebc48eef6cdae2805b7f528a3713c9c6648d3
TheKasman <kasapidisandrew@gmail.com>
2026-02-18 00:45:55 +0000

    forgot some important stuff... like listing the filename as f and returning things

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -7,9 +7,10 @@ import json
 def serialize_and_save_to_file(data, filename):
     """I'm super Serialized"""
     with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, filename)
+        json.dump(data, f)
+
 
 def load_and_deserialize(filename):
     """Okay maybe we need to deserialize the situation"""
     with open(filename, "r", encoding="utf-8") as f:
-        json.load(filename)
+        return json.load(f)
