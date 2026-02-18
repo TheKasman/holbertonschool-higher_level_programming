b9bafee74aab45b45d6504a36f01f3a6996288e3
TheKasman <kasapidisandrew@gmail.com>
2026-02-18 01:34:26 +0000

    forgot the exceptions

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,12 +8,20 @@ import json
 def convert_csv_to_json(filename):
     """read things in the csv and convert to json"""
     #  Read CSV
-    with open(filename, newline="", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        #  List of dictionaries
-        data = [row for row in reader] 
+    try:
+        with open(filename, newline="", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            #  List of dictionaries
+            data = [row for row in reader]
 
-    #  Convert to dictionary and JSON
-    json_data = json.dumps(data, indent=4)
-    with open("data.json", "w", encoding="utf-8") as f:
-        f.write(json_data)
+        #  Convert to dictionary and JSON
+        json_data = json.dumps(data, indent=4)
+        with open("data.json", "w", encoding="utf-8") as f:
+            f.write(json_data)
+
+        # Success!
+        return True
+    except FileNotFoundError:
+        return False
+    except Exception:
+        return False
