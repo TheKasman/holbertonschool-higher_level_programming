0af9d94aa605323c320cebe6997a67d7b77511de
TheKasman <kasapidisandrew@gmail.com>
2026-02-18 00:40:02 +0000

    question 0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""No pickles here, neither is there dill"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """I'm super Serialized"""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, filename)
+
+def load_and_deserialize(filename):
+    """Okay maybe we need to deserialize the situation"""
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(filename)
