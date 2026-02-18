7bc924d001110d8ba833115f380e886f29542e17
TheKasman <kasapidisandrew@gmail.com>
2026-02-18 01:30:53 +0000

    CSV

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""CSV -> JSON"""
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """read things in the csv and convert to json"""
+    #  Read CSV
+    with open(filename, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        #  List of dictionaries
+        data = [row for row in reader] 
+
+    #  Convert to dictionary and JSON
+    json_data = json.dumps(data, indent=4)
+    with open("data.json", "w", encoding="utf-8") as f:
+        f.write(json_data)
