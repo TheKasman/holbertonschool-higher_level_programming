41bde8b23bad7dc87e8559d8402b844cfdbed8a1
TheKasman <kasapidisandrew@gmail.com>
2026-02-24 02:28:18 +0000

    basic auth fix

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -33,7 +33,7 @@ users = {
 #  Basic auth
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and check_password_hash(users.get(username),
+    if username in users and check_password_hash(users["password"],
                                                  password):
         return username
     return None
