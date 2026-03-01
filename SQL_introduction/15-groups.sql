34e92dc3762f4ea088f0c251aded311015ad0931
TheKasman <kasapidisandrew@gmail.com>
2026-03-01 00:26:08 +0000

    forgot to order it

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,6 @@
 -- grouping scores together
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY
+    score
+ORDER BY score DESC;
