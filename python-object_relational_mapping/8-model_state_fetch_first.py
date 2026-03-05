a2a70cb0490c9f1ab19d95685f3dbb99fc48af6a
TheKasman <kasapidisandrew@gmail.com>
2026-03-06 08:26:54 +1100

    man this is way easier

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""State class definition for use in SQLAlchemy"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    #  MYSQL connection
+    USER_NAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE = sys.argv[3]
+
+    engine = create_engine(f"mysql+mysqldb://{USER_NAME}:{PASSWORD}"
+                           f"@localhost:3306/{DATABASE}")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #  This is it. this is the SQL query now.
+    state = session.query(State).order_by(State.id).first()
+
+    if state:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
