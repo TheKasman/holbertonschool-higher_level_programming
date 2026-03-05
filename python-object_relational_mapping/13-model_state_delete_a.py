d88d6dddff2436ebcc980d976ff605ebedeb51a8
TheKasman <kasapidisandrew@gmail.com>
2026-03-06 09:22:50 +1100

    you delete an entry from the SESSION. not the object

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""State class definition for use in SQLAlchemy"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    #  MYSQL connection
+    USER_NAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE = sys.argv[3]
+
+    engine = create_engine(f"mysql+mysqldb://{USER_NAME}:{PASSWORD}"
+                           f"@localhost:3306/{DATABASE}")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #  This is it. this is the SQL query now.
+    states = session.query(State)\
+        .filter(State.name.like("%a%"))\
+        .order_by(State.id).all()
+
+    for state in states:
+        session.delete(state)
+
+    states.commit()
