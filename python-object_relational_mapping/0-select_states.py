c0c3d2011b294a504da59733628e8d3bb6750882
TheKasman <kasapidisandrew@gmail.com>
2026-03-05 16:18:00 +1100

    first one's done

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_usa"""
+
+import sys
+import MySQLdb
+
+def main():
+    """Get arguments: username, password, database"""
+    # TIME FOR BOILERPLATE
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        return
+
+    user = sys.argv[1]
+    passwrd = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host= 'localhost',
+        port= 3306,
+        user= user,
+        passwd=passwrd,
+        db=database
+    )
+
+    # make a cursor
+    cur = db.cursor()
+
+    # THE ACTUAL THING THAT'S IMPORTANT
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # MORE BOILERPLATE
+    for row in cur.fetchall():
+        print(row)
+
+if __name__ == "__main__":
+    main()
