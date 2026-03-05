7bfaa65620ea068b912a5e3972a505b2d8adff8a
TheKasman <kasapidisandrew@gmail.com>
2026-03-06 08:18:57 +1100

    added sys and changed connection details to inputted details

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """State class definition for use in SQLAlchemy"""
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
     #  MYSQL connection
-    USER_NAME = 'root'
-    PASSWORD = 'root'
-    DATABASE = 'hbtn_0e_6_usa'
+    USER_NAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE = sys.argv[3]
 
     engine = create_engine(f"mysql+mysqldb://{USER_NAME}:{PASSWORD}"
                            f"@localhost:3306/{DATABASE}")
