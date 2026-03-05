13c3c82653688655af1db809105f32dbca46f21d
TheKasman <kasapidisandrew@gmail.com>
2026-03-06 08:45:32 +1100

    here we go

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""State class definition for use in SQLAlchemy"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    #  MYSQL connection
+    USER_NAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE = sys.argv[3]
+    NAME = sys.argc[4]
+
+    engine = create_engine(f"mysql+mysqldb://{USER_NAME}:{PASSWORD}"
+                           f"@localhost:3306/{DATABASE}")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #  This is it. this is the SQL query now.
+    state = session.query(State).filter(State.name == NAME)
+
+    if state:
+        print(f"{state.id}")
