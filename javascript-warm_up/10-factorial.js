e0345dbdb2b6ec054f242283b003da498e251cdd
TheKasman <kasapidisandrew@gmail.com>
2026-03-16 17:22:06 +1100

    factorial but recursive

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num1 = parseInt(args[0], 10);
+
+function factorial (n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num1));
