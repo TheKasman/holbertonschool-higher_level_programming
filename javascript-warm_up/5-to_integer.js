5719ca3f6acad83a7bc022e49f16805fc63f392f
TheKasman <kasapidisandrew@gmail.com>
2026-03-16 16:46:03 +1100

    isNaN is a thing

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const num = parseInt(args[0], 10); //  turn to base 10
-if (num === 'NaN') {
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + num);
