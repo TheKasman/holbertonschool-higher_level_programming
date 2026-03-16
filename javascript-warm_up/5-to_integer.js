9991b669d01b08aa56c9ebdc8c384306618d7f4b
TheKasman <kasapidisandrew@gmail.com>
2026-03-16 16:43:57 +1100

    better output

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const num = parseInt(args[0], 10); //  turn to base 10
-console.log('My number: ' + num);
+if (num === 'NaN') {
+  console.log('Not a Number');
+} else {
+  console.log('My number: ' + num);
+}
