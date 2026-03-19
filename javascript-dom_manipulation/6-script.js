f2edadea962ff63fda91655c54b5641af9efe6b5
TheKasman <kasapidisandrew@gmail.com>
2026-03-19 14:44:33 +1100

    star wars

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const character = document.getElementById('character');
+  
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+        character.textContent = data.name;
+    })
+    .catch(error => console.error('Error fetching character:', error));
+});
