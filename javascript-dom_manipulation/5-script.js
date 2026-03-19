04545de4c0a11ebf0bf5c658e952334e30c0284e
TheKasman <kasapidisandrew@gmail.com>
2026-03-19 17:53:46 +1100

    selected the correct thing this time

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const header = document.getElementById('update_header');
-  
-  header.addEventListener('click', () => {
+  const div = document.getElementById('update_header'); // the clickable div
+  const header = document.querySelector('header');      // the header to update
+
+  div.addEventListener('click', () => {
     header.textContent = 'New Header!!!';
   });
 });
