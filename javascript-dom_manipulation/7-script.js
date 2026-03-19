972cf890d58b06d692034959ef6b8718da4d7d4d
TheKasman <kasapidisandrew@gmail.com>
2026-03-19 14:56:19 +1100

    question 7 done

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const moviesList = document.getElementById('list_movies');
+  
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      data.results.forEach(film => {
+        const li = document.createElement('li');
+        li.textContent = film.title;
+        moviesList.appendChild(li);            
+      });
+    })
+    .catch(error => console.error('Error fetching movies:', error));
+});
